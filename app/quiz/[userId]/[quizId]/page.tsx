e1b66e0369107bb1663ae784851efcbb0ddfd227
jeymes <jsvpn2017@gmail.com>
2024-11-11 23:34:31 -0300

    created salve in quiz relations reponses

--- app/quiz/[userId]/[quizId]/page.tsx
+++ app/quiz/[userId]/[quizId]/page.tsx
@@ -2,39 +2,103 @@
 
 import { useParams } from 'next/navigation';
 import { useState } from 'react';
-import { Button, Typography, Container, Paper, Box, Grid } from '@mui/material';
+import { Paper, Box } from '@mui/material';
 import { useQuizStore } from '@/app/zustand/StoreQuiz/store';
 import Modelo01Preview from '@/app/create-quiz/models/models01/model01-preview';
 import Modelo02Preview from '@/app/create-quiz/models/models02/model02-preview';
 import Modelo03Preview from '@/app/create-quiz/models/models03/model03-preview';
 import Modelo04Preview from '@/app/create-quiz/models/models04/model04-preview';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+// Tipos de dados para o quiz (ajuste conforme sua estrutura real)
+interface QuizModel {
+    model: string;
+    options: {
+        option: string;
+        image: string;
+    };
+    isFullWidth: boolean;
+}
+
+interface QuizPageData {
+    models: QuizModel[];
+}
+
+interface QuizData {
+    pages: QuizPageData[];
+    edges: {
+        source: string;
+        target: string;
+    }[];
+}
+
+interface Responses {
+    question: string;
+    selectedOption: string;
+    model: string;
+}
 
 const QuizPage = () => {
     const params = useParams();
     const { quizData } = useQuizStore();
     const [currentPageIndex, setCurrentPageIndex] = useState<number>(0);
-    const [answer, setAnswer] = useState<string>(''); // Resposta selecionada
+    const [currentProgress, setCurrentProgress] = useState<number>(1);
 
-    console.log("quizData", quizData);
+    const { setValue, handleSubmit, watch } = useForm<{
+        responses: Responses[];
+    }>({
+        defaultValues: {
+            responses: []  // Mantém as respostas de cada página
+        }
+    });
 
     if (!quizData) return <div>Carregando...</div>;
 
     const currentPage = quizData.pages[currentPageIndex];
 
-    const handleNext = () => {
-        if (quizData && currentPage) {
-            // Encontra a próxima página com base no `source` e `target` de `edges`
-            const nextEdge = quizData.edges.find(edge => edge.source === String(currentPageIndex + 1));
-            if (nextEdge) {
-                const nextPageIndex = parseInt(nextEdge.target, 10) - 1; // Converte target para índice da página
+    // Função para verificar se o modelo "model03" está presente na página
+    const getQuestionFromModel03 = () => {
+        const model03 = currentPage.models.find(model => model.model === 'model03');
+        if (model03) {
+            return model03.options.option;
+        }
+        return '';
+    };
+
+    const handleNext = (selectedOption: string, model: string) => {
+
+        let question = getQuestionFromModel03();
+
+        if (!question) {
+            question = "Pergunta Padrão";
+        }
+
+        setValue(`responses.${currentPageIndex}`, {
+            question,
+            selectedOption,
+            model
+        });
+
+        // Chama a submissão de dados de forma assíncrona, se necessário
+        handleSubmit(onSubmit)(); // Garante que a resposta é salva antes de avançar
+
+
+        const nextEdge = quizData.edges.find(edge => edge.source === String(currentPageIndex + 1));
+
+        if (nextEdge) {
+            const nextPageIndex = parseInt(nextEdge.target, 10) - 1;
+            if (nextPageIndex >= 0 && nextPageIndex < quizData.pages.length) {
                 setCurrentPageIndex(nextPageIndex);
-                setAnswer(''); // Resetar a resposta ao mudar de página
+                // Incrementa o progresso corretamente
+                setCurrentProgress(prevProgress => prevProgress + 1);
+            } else {
+                console.log('Fim do quiz!');
             }
         }
     };
 
-    const handleAnswerChange = (selectedAnswer: string) => {
-        setAnswer(selectedAnswer);
+    const onSubmit: SubmitHandler<{ responses: Responses[] }> = (data) => {
+        console.log("Respostas atualizadas:", data.responses);
     };
 
     return (
@@ -58,14 +122,7 @@ const QuizPage = () => {
                     alignItems: 'center',
                     flexDirection: 'column'
                 }}
-
             >
-                {/* <Typography variant="h3" gutterBottom align="center">{quizData.title}</Typography> */}
-                {/* <Typography variant="h6" paragraph align="center">Página {currentPageIndex + 1} de {quizData.pages.length}</Typography> */}
-
-                {/* <Typography variant="h5" paragraph>{currentPage.question}</Typography> */}
-
-                {/* Exibir opções dinamicamente */}
                 <Box
                     sx={{
                         width: '60%',
@@ -77,17 +134,18 @@ const QuizPage = () => {
                     }}
                     mb={3}
                 >
-                    {currentPage.models.map((model, index) => (
+                    {currentPage.models.map((model: any, index: any) => (
                         <Box
-                            sx={
-                                {
-                                    width: model.isFullWidth ? '90%' : 'calc(23%)',
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    padding: 1,
-                                }
-                            }
+                            key={index}
+                            sx={{
+                                width: model.isFullWidth ? '90%' : 'calc(23%)',
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                padding: 1,
+                                cursor: 'pointer'
+                            }}
+                            onClick={() => handleNext(model.options.option, model.model)}
                         >
                             {model.model === 'model01' && (
                                 <Modelo01Preview imageUrl={model.options.image} />
@@ -96,7 +154,7 @@ const QuizPage = () => {
                             {model.model === 'model02' && (
                                 <Modelo02Preview
                                     totalPages={quizData?.pages.length}
-                                    currentPage={currentPageIndex}
+                                    currentPage={currentProgress}
                                 />
                             )}
 
@@ -105,23 +163,11 @@ const QuizPage = () => {
                             )}
 
                             {model.model === 'model04' && (
-                                <Modelo04Preview
-                                    imageUrl={model.options.image}
-                                    option={model.options.option || `Opção-${index}`}
-                                />
+                                <Modelo04Preview imageUrl={model.options.image} option={model.options.option} />
                             )}
                         </Box>
                     ))}
                 </Box>
-
-                <Typography variant="body1" color="textSecondary" align="center">{currentPage.footer}</Typography>
-
-                {/* <Box display="flex" justifyContent="space-between" alignItems="center" mt={4}>
-                    <Typography variant="body2" color="textSecondary">{currentPage.progress}</Typography>
-                    <Button variant="contained" onClick={handleNext} color="primary" size="large">
-                        Próxima
-                    </Button>
-                </Box> */}
             </Paper>
         </Box>
     );
